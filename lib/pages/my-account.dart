7fef281c55bf456f8f4f007236d5d1ec55a212b9
JezzDiego <oliveirajesse50@gmail.com>
2022-06-20 00:11:25 -0300

    feat: my account screen

---
+++ lib/pages/my-account.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class MyAccount extends StatefulWidget {
+  const MyAccount({Key? key}) : super(key: key);
+
+  @override
+  State<MyAccount> createState() => _MyAccountState();
+}
+
+class _MyAccountState extends State<MyAccount> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text('Minha Conta'),
+        centerTitle: false,
+        backgroundColor: Colors.blue,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 100,
+                  height: 100,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSebLzQHeNYV0TVtz49fMdUANYfv1tcX9off2lbzaUFhqGAk-6zjQr6xhdyEnAY343KW2Y&usqp=CAU")
+                      )
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: const [
+                      Text('Luana Silva',
+                      style: TextStyle(fontSize: 25)
+                      ),
+                      Text('9 9123-4567'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            GestureDetector(
+              child: Padding(
+                padding: const EdgeInsetsDirectional.only(top: 60, start: 20),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Row(
+                        children: const [
+                          Icon(Icons.shopping_bag),
+                          Padding(
+                            padding: EdgeInsetsDirectional.only(start: 20),
+                            child: Text('Meus Pedidos',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 40.0, left: 10.0),
+                        child: Row(
+                          children: const [
+                            Icon(Icons.person),
+                            Padding(
+                              padding: EdgeInsetsDirectional.only(start: 20),
+                              child: Text('Minhas Informações',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
