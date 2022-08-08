711b3fae8b80ef4f1815e8c62c48bfa7428218dd
JezzDiego <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>
2022-08-08 14:54:34 -0300

    chore: initial configuration os orders screen

--- lib/pages/my_orders.dart
+++ lib/pages/my_orders.dart
@@ -16,6 +16,87 @@ class _MyOrdersState extends State<MyOrders> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
+      body: ListView(
+        children: [
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 16, 10, 15),
+            padding: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xFFE0E0E0),
+            ),
+            child: const TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search),
+                hintText: "Pesquisar",
+                border: InputBorder.none,
+                hintStyle: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "25 de Dezembro",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      Text(
+                        "R\$ 450.00",
+                        style: TextStyle(fontSize: 20),
+                      )
+                    ]),
+                Padding(
+                  padding: const EdgeInsets.only(top: 12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Entregue",
+                        style: TextStyle(
+                            fontSize: 16, color: Colors.greenAccent[700]),
+                      ),
+                      const Text(
+                        "#14124",
+                        style: TextStyle(color: Colors.grey, fontSize: 16),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16),
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 100,
+                        height: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(6),
+                          border: Border.all(
+                            color: Colors.grey
+                          ),
+                          image: const DecorationImage(
+                            fit: BoxFit.fill,
+                            image: NetworkImage(
+                                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSebLzQHeNYV0TVtz49fMdUANYfv1tcX9off2lbzaUFhqGAk-6zjQr6xhdyEnAY343KW2Y&usqp=CAU"),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
