452450775e425f258c4b62eb4efdf9d532370e1e
JezzDiego <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>
2022-08-22 15:49:43 -0300

    chore: creating the instance on users collection

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -1,4 +1,3 @@
-import 'package:araplantas_mobile/components/initial_screen.dart';
 import 'package:araplantas_mobile/pages/login.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -17,24 +16,47 @@ class _SignUpState extends State<SignUp> {
   final _formKey = GlobalKey<FormState>();
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
   TextEditingController nameController = TextEditingController();
   TextEditingController numberController = TextEditingController();
 
   bool loading = false;
 
-  _logIn() async {
+  Future _signUp() async {
     setState(() {
       loading = true;
     });
 
     try {
-      await FirebaseAuth.instance.signInWithEmailAndPassword(
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: emailController.text, password: passwordController.text);
-      Navigator.pushReplacement(context, MaterialPageRoute(
-        builder: (context) {
-          return const InitialScreen();
-        },
-      ));
+
+      await FirebaseFirestore.instance.collection('users').add({
+        'email': emailController.text,
+        'name': nameController.text,
+        'cel': numberController.text,
+      });
+
+      await showDialog(
+          context: context,
+          builder: ((context) => AlertDialog(
+                title: const Text("Cadastrado com sucesso"),
+                content: const Text(
+                    "Sua conta foi criada, agora você pode entrar com suas credenciais"),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(context, MaterialPageRoute(
+                          builder: (context) {
+                            return const Login();
+                          },
+                        ));
+                      },
+                      child: const Text('OK'))
+                ],
+              )));
+
+      //Navigator.of(context).pop();
     } on FirebaseAuthException catch (e) {
       showDialog(
           context: context,
@@ -97,69 +119,102 @@ class _SignUpState extends State<SignUp> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      const SizedBox(height: 40),
+                      const SizedBox(height: 30),
                       const Text(
                         "Nome",
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 20),
                       ),
                       SizedBox(
-                        height: 30,
+                        height: 45,
                         child: buildTextFormField(
                             false, nameController, TextInputType.name),
                       ),
-                      const SizedBox(height: 40),
+                      const SizedBox(height: 30),
                       const Text(
                         "Email",
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 20),
                       ),
                       SizedBox(
-                        height: 30,
+                        height: 45,
                         child: buildTextFormField(
                             false, emailController, TextInputType.emailAddress),
                       ),
-                      const SizedBox(height: 40),
+                      const SizedBox(height: 30),
                       const Text(
                         "Celular",
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 20),
                       ),
                       SizedBox(
-                        height: 30,
+                        height: 45,
                         child: buildTextFormField(
                             false, numberController, TextInputType.phone),
                       ),
-                      const SizedBox(height: 40),
+                      const SizedBox(height: 30),
                       const Text(
                         "Senha",
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 20),
                       ),
                       SizedBox(
-                        height: 30,
-                        child: buildTextFormField(true, passwordController,
-                            TextInputType.visiblePassword),
+                        height: 45,
+                        child: TextFormField(
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: passwordController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Campo obrigatório";
+                            } else if (value.length < 3) {
+                              return "O campo deve ter mais de 3 caracteres";
+                            } else if (confirmPasswordController.text !=
+                                passwordController.text) {
+                              return "As senha devem ser iguais";
+                            }
+                            return null;
+                          },
+                        ),
                       ),
-                      const SizedBox(height: 40),
+                      const SizedBox(height: 30),
                       const Text(
                         "Confirmar Senha",
                         style: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 20),
                       ),
                       SizedBox(
-                        height: 30,
-                        child: buildTextFormField(true, passwordController,
-                            TextInputType.visiblePassword),
+                        height: 45,
+                        child: TextFormField(
+                          obscureText: true,
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: confirmPasswordController,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return "Campo obrigatório";
+                            } else if (value.length < 3) {
+                              return "O campo deve ter mais de 3 caracteres";
+                            } else if (confirmPasswordController.text !=
+                                passwordController.text) {
+                              return "As senha devem ser iguais";
+                            }
+                            return null;
+                          },
+                        ),
                       ),
-                      const SizedBox(height: 40),
+                      const SizedBox(height: 30),
                       if (!loading)
                         Center(
                           child: SizedBox(
                             width: 240,
                             height: 50,
                             child: TextButton(
-                              onPressed: () {},
+                              onPressed: () {
+                                if (_formKey.currentState != null &&
+                                    _formKey.currentState!.validate()) {
+                                  _signUp();
+                                }
+                              },
                               child: const Text("Cadastrar",
                                   style: TextStyle(fontSize: 24)),
                               style: TextButton.styleFrom(
@@ -202,6 +257,33 @@ class _SignUpState extends State<SignUp> {
         ),
       ),
     );
+  }
+
+  String verifyContent(String e) {
+    String message = "Ocorreu um erro desconhecido, tente novamente mais tarde";
+    switch (e) {
+      case 'email-already-in-use':
+        message = "Este email já está cadastrado";
+        break;
+
+      case 'invalid-email':
+        message = "O email que você digitou é inválido";
+        break;
+
+      case 'operation-not-allowed':
+        message = "Essa operação não é inválida";
+        break;
+
+      case 'weak-password':
+        message = "A senha que você digitou é muito fraca";
+        break;
+
+      default:
+        message = "Ocorreu um erro desconhecido, tente novamente mais tarde";
+        break;
+    }
+
+    return message;
   }
 }
 
