6004cc02bb06952ab0e0fb81df46405bbd388222
JezzDiego <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>
2022-08-22 15:50:16 -0300

    chore: removing unused imports

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
