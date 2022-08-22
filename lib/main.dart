72827c2acc8800207d7949d7fc9cea430049fa6b
JezzDiego <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>
2022-08-21 23:13:27 -0300

    feat: initializating firebase app on app start

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,30 @@
 import 'package:araplantas_mobile/pages/login.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const HomePage());
+  runApp(HomePage());
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
+
+  final _initialization = Firebase.initializeApp();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'inicial',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.grey,
-      ),
-      home: const Login(),
-    );
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          return MaterialApp(
+            title: 'inicial',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.grey,
+            ),
+            home: const Login(),
+          );
+        });
   }
 }
