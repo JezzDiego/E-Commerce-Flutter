80371279c026f5590eaae2fef4c607635ae3795f
JezzDiego <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>
2022-08-27 16:45:01 -0300

    feat: google sign in class

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -256,8 +256,8 @@ class _LoginState extends State<Login> {
                       ),
                       Center(
                         child: SizedBox(
-                          width: 70,
-                          height: 70,
+                          width: 60,
+                          height: 60,
                           child: IconButton(
                               onPressed: () {
                                 _loginWithGoogle();
