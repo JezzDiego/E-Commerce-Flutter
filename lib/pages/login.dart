c303fb6b43f20864ed0ef65db948a5dce902e223
JezzDiego <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>
2022-08-27 18:22:41 -0300

    chore: removing unused imports

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,11 +1,9 @@
 import 'package:araplantas_mobile/components/google_sign_in.dart';
 import 'package:araplantas_mobile/components/initial_screen.dart';
 import 'package:araplantas_mobile/pages/sign_up.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import "package:flutter/material.dart";
 import 'package:google_fonts/google_fonts.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:provider/provider.dart';
 
 class Login extends StatefulWidget {
