0173102d136eaf376b435b4f569ba53f12ba1066
JezzDiego <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>
2022-08-28 21:39:08 -0300

    chore: changing static texto to texfield

--- lib/pages/my_info.dart
+++ lib/pages/my_info.dart
@@ -24,64 +24,90 @@ class _MyInfoState extends State<MyInfo> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: ListView(
-          children: [
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: SizedBox(
-                    width: 800,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Pessoais",
-                          style: GoogleFonts.inter(
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                child: SizedBox(
+                  width: 800,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Pessoais",
+                        style: GoogleFonts.inter(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
                         ),
-                        const SizedBox(height: 40),
-                        buildInfo(
-                            "Nome Completo",
-                            user.displayName != null
-                                ? "${user.displayName}"
-                                : "Não cadastrado"),
-                        const SizedBox(height: 40),
-                        buildInfo("Email", user.email!),
-                        const SizedBox(height: 40),
-                        buildInfo(
-                            "Celular",
-                            user.phoneNumber == null
-                                ? "Não cadastrado"
-                                : "${user.phoneNumber}"),
-                        const SizedBox(height: 40),
-                        Text(
-                          "Endereço",
-                          style: GoogleFonts.inter(
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold,
-                          ),
+                      ),
+                      const SizedBox(height: 40),
+                      buildInfo(
+                          "Nome Completo",
+                          user.displayName != null
+                              ? "${user.displayName}"
+                              : "Não cadastrado"),
+                      const SizedBox(height: 40),
+                      buildInfo("Email", user.email!),
+                      const SizedBox(height: 40),
+                      buildInfo(
+                          "Celular",
+                          user.phoneNumber == null
+                              ? "Não cadastrado"
+                              : "${user.phoneNumber}"),
+                      const SizedBox(height: 40),
+                      Text(
+                        "Endereço",
+                        style: GoogleFonts.inter(
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
                         ),
-                        const SizedBox(height: 40),
-                        buildInfo("CEP", "57123456"),
-                        const SizedBox(height: 40),
-                        buildInfo("Celular", "9 9123-4567"),
-                        const SizedBox(height: 40),
-                        buildInfo("Rua", "Rua das Flores"),
-                        const SizedBox(height: 40),
-                        buildInfo("Bairro", "Lorem Ipsum"),
-                        const SizedBox(height: 40),
-                        buildInfo("Número da Casa", "123"),
-                        const SizedBox(height: 40),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(height: 40),
+                      buildTextFieldInfo("CEP"),
+                      const SizedBox(height: 40),
+                      buildTextFieldInfo("Rua"),
+                      const SizedBox(height: 40),
+                      buildTextFieldInfo("Bairro"),
+                      const SizedBox(height: 40),
+                      buildTextFieldInfo("Número da Casa"),
+                      const SizedBox(height: 40),
+                    ],
                   ),
-                )
-              ],
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  buildTextFieldInfo(String campo) {
+    return Container(
+      width: 900,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color(0xffF5F5F5),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              campo,
+              style: GoogleFonts.inter(
+                color: Colors.grey,
+                fontSize: 15,
+              ),
+            ),
+            TextField(
+              style: GoogleFonts.inter(
+                fontSize: 22,
+              ),
             ),
           ],
         ),
