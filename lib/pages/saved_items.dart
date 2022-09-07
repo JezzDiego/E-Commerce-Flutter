bec08cf7be21aac21cac8bb692e8965b559595ec
JezzDiego <oliveirajesse50@gmail.com>
2022-09-07 19:35:11 -0300

    chore: removing search bar from saved items screen

--- lib/pages/saved_items.dart
+++ lib/pages/saved_items.dart
@@ -36,49 +36,27 @@ class _SavedItemsState extends State<SavedItems> {
   }
 
   buildBody() {
-    return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.fromLTRB(10, 16, 10, 30),
-            padding: const EdgeInsets.all(6),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color(0xFFE0E0E0),
-            ),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.search),
-                hintText: "Pesquisar",
-                border: InputBorder.none,
-                hintStyle: GoogleFonts.inter(
-                  fontSize: 20,
-                  color: Colors.grey,
-                ),
-              ),
-            ),
-          ),
-          StreamBuilder<List<Item>>(
-              stream: readSavedItems(),
-              builder: ((context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Text("Algo deu errado");
-                } else if (snapshot.hasData) {
-                  final items = snapshot.data;
-                  return items == null
-                      ? const Center(
-                          child: Text(
-                              "Ao salvar algum item você poderá acessá-lo aqui"))
-                      : ListView(
-                          shrinkWrap: true,
-                          children: items.map(buildSavedItems).toList(),
-                        );
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              }))
-        ],
-      ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: StreamBuilder<List<Item>>(
+          stream: readSavedItems(),
+          builder: ((context, snapshot) {
+            if (snapshot.hasError) {
+              return const Text("Algo deu errado");
+            } else if (snapshot.hasData) {
+              final items = snapshot.data;
+              return items == null
+                  ? const Center(
+                      child: Text(
+                          "Ao salvar algum item você poderá acessá-lo aqui"))
+                  : ListView(
+                      shrinkWrap: true,
+                      children: items.map(buildSavedItems).toList(),
+                    );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          })),
     );
   }
 
