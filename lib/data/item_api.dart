7d0bef0665236904b8043181869cf7c5d46d46a6
antonyg5 <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>
2022-12-27 09:23:18 -0300

    feat:item api

--- lib/data/item_api.dart
+++ lib/data/item_api.dart
@@ -1,49 +1,49 @@
 import 'dart:convert';
 
-import 'package:araplantas_mobile/models/user.dart';
+import 'package:araplantas_mobile/models/item.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
-class UserApi {
+class ItemApi {
   final String _baseUrl = dotenv.env['API_URL']!;
 
-  Future<List<User>> findAll() async {
-    Uri url = Uri.http(_baseUrl, "/users");
+  Future<List<Item>> findAll() async {
+    Uri url = Uri.http(_baseUrl, "/items");
     final Response response = await http.get(url);
     if (response.statusCode == 200) {
       List<dynamic> body = jsonDecode(response.body);
-      List<User> users =
-          body.map((dynamic item) => User.fromJson(item)).toList();
-      return users;
+      List<Item> items =
+          body.map((dynamic item) => Item.fromJson(item)).toList();
+      return items;
     } else {
-      throw "Can't get users.";
+      throw "Can't get items.";
     }
   }
 
-  Future<User> findById(String id) async {
-    Uri url = Uri.http(_baseUrl, "/users/$id");
+  Future<Item> findById(String id) async {
+    Uri url = Uri.http(_baseUrl, "/items/$id");
     final Response response = await http.get(url);
     if (response.statusCode == 200) {
-      return User.fromJson(jsonDecode(response.body));
+      return Item.fromJson(jsonDecode(response.body));
     } else {
-      throw "Can't get user.";
+      throw "Can't get item.";
     }
   }
 
-  Future<http.Response> create(User user) {
-    Uri url = Uri.http(_baseUrl, "/users");
+  Future<http.Response> create(Item item) {
+    Uri url = Uri.http(_baseUrl, "/items");
     return http.post(
       url,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
-      body: user.toJson(),
+      body: item.toJson(),
     );
   }
 
   Future<http.Response> delete(String id) {
-    Uri url = Uri.http(_baseUrl, "/users/$id");
+    Uri url = Uri.http(_baseUrl, "/items/$id");
     return http.delete(url);
   }
 }
