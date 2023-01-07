353bde57be31189ea217020dea0d2ee60214ce87
JezzDiego <oliveirajesse50@gmail.com>
2023-01-07 20:21:06 -0300

    fix issues when try to connect to api locally

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,19 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import 'models/user.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
+      name: 'araplantas-mobile',
       options: FirebaseOptions(
-    appId: dotenv.env['FIREBASE_APP_ID']!,
-    apiKey: dotenv.env['FIREBASE_API_KEY']!,
-    messagingSenderId: dotenv.env['FIREBASE_SENDER_ID']!,
-    projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
-  ));
+        appId: dotenv.env['FIREBASE_APP_ID']!,
+        apiKey: dotenv.env['FIREBASE_API_KEY']!,
+        messagingSenderId: dotenv.env['FIREBASE_SENDER_ID']!,
+        projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+      ));
   runApp(const HomePage());
 }
 
